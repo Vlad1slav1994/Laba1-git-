00d7a66695fcdef55240104ef124c9eaaac74bcf
Влад Фирсов <firsvld@mail.ru>
2019-10-02 15:08:03 +0300

    Gitnew

--- Laba1(git)/Program.cs
+++ Laba1(git)/Program.cs
@@ -10,6 +10,12 @@ namespace Laba1_git_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
